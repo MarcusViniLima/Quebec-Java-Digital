c758f8c8dd0bcfd7dead72b3c2b87bf0733ba1e1
Marcus vinicius Lameu lima <91769337+MarcusViniLima@users.noreply.github.com>
2022-09-25 14:49:36 -0300

    Iniciando o projeto

---
+++ src/main/java/com/br/marcus/meuPrimeiroProjeto/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,14 @@
+package com.br.marcus.meuPrimeiroProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Bem vindo ao mundo spring!");
+	}
+
+}
